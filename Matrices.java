906f8d5742d4dc83320db2dd5c9913ffe65bffff
Ared <ared230000@gmail.com>
2024-10-16 12:21:15 -0600

    Imprimiendo matriz irregular

--- Matrices.java
+++ Matrices.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Matrices
  */
 public class Matrices {
+    public static BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+    public static String entrada;
 
     public static void imprimirMatriz(int[][] m){ // m es parametro
         for (int i = 0; i < m.length; i++ ){
@@ -11,10 +17,13 @@ public class Matrices {
             System.out.println();
         }
     }
+
     
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws IOException {
         int[][] matriz1 = new int[3][3];
         int numero = 1;
+        int renglones;  
 
         for (int i = 0; i < matriz1.length; i++){
             for (int j = 0; j < matriz1[i].length; j++){
@@ -24,6 +33,19 @@ public class Matrices {
         }
         System.out.println("Los valores de matriz1 son");
         imprimirMatriz(matriz1);
+
+        // Manipulando matriz irregular 
+        int[][] matriz2;
+        System.out.println("Escribe el total de renglones: ");
+        entrada = bufer.readLine();
+        renglones = Integer.parseInt(entrada);
+        // Construyendo la matriz2
+        matriz2 = crearMatrizIrregular(renglones);
+        // Colocar valores en matriz2
+        matriz2 = llenarMatrizIrregular(matriz2);
+        // Imprimiendo la matriz
+        System.out.println("Valores de la matriz irregular: ");
+        imprimirMatriz(matriz2);
             
     }
 }
